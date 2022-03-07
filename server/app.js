6a1b27f30b98b06b7c3575509f3ee04cd0ef7ca4
Daniel Chua <53444908+DCH25@users.noreply.github.com>
2022-03-06 22:08:31 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const express_gql = require("express-graphql");
+const mongoose = require("mongoose");
+const {
+    GraphQLID,
+    GraphALString,
+    GraphQLList,
+    GraphQLType,
+    GraphQLSchema,
+    GraphQLNonNull,
+    GraphQLObjectType
+} = require("graphql");
+
+require("dotenv").config();
+
+var app = express();
+var cors = require("cors");
+
+
+
+// Hide credentials from public repository
+let mdb_password = process.env.MDB_PASSWORD;
+let db_name = process.env.DB_NAME;
+
+
+// Connect to DB
+mongoose
+    .connect("mongodb+srv://bestestcscc09group:" + mdb_password + "@cluster0.ykdmy.mongodb.net/" + db_name + "?retryWrites=true&w=majority")
+    .then(() => console.log("Connected to database..."))
+    .catch(err => console.log(err));
