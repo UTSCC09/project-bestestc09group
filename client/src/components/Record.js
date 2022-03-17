e8f9d8b7c4f1e40cf9cf3b1c5fc58b738b051346
Daniel Chua <53444908+DCH25@users.noreply.github.com>
2022-03-17 13:07:39 -0400

    Edit Tuning Toggle

--- client/src/components/Record.js
+++ client/src/components/Record.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import { React, useState } from 'react';
 
 /* ----- Styling ----- */
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import TuningEdit from './TuningEdit';
 import Playlist from './Playlist';
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/esm/Collapse';
 
 const Record = ({tuning}) => {
+    const [show, setShow] = useState(false);
+
     return (
         <Container fluid>
             <Row>
                 <Playlist/>
             </Row>
+            
+            <Button className="mb-3" onClick={() => setShow(!show)}>Edit Tuning</Button>
             <Row>
-                <TuningEdit tuning={tuning}/>
+                <Collapse in={show}>
+                    <Container fluid>
+                        <TuningEdit tuning={tuning}/>
+                    </Container>
+                </Collapse>
             </Row>
         </Container>
     );
