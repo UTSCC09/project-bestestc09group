2efb5a25d0a6cada46843232906f3aaa979c6cb5
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2022-04-02 14:44:04 -0400

    [web-player-config] Trying to convert track cards to collapsables
    - Make it so only sends request when expanding card.
    - May solve problem of 'Too many requests'

--- client/src/components/Track.js
+++ client/src/components/Track.js
@@ -10,14 +10,19 @@ const Track = ({data}) => {
         <Container className='mb-3'>
             <Card>
                 <Card.Body className='row'>
-                    <div className='col-auto'>
+                    {/* <div className='col-auto bg-dark'>
                         <iframe src={"https://open.spotify.com/embed/track/" + data.id} width="80" height="80" frameBorder="0"></iframe>
-                    </div>
+                    </div> */}
                     <div className='col'>
                         <Card.Title>{data.name}</Card.Title>
                         <Card.Text>{data.artists.map((artist) => {
                             return artist.name;
                         }).join(',')}</Card.Text>
+                    </div>
+                    <div className='col d-flex align-items-center justify-content-end'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                        </svg>
                     </div>
                     {/* <Button className='me-2'>Like</Button> */}
                     {/* <Button>Dislike</Button> */}
