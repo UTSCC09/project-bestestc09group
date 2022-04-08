7f0bf6d5006036d4620ddf00cdc8bf5e5afcd31f
Daniel Chua <53444908+DCH25@users.noreply.github.com>
2022-04-08 15:33:43 -0400

    Update name

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,7 +12,7 @@ const Header = ({ authHandler, auth, logout }) => {
             <Container fluid className="mb-3">
                 <Navbar bg="light">
                     <div className='col'>
-                        <Link to="/" className='text-decoration-none'><Navbar.Brand className='fw-bold'>DiscoverWeeklyU</Navbar.Brand></Link>
+                        <Link to="/" className='text-decoration-none'><Navbar.Brand className='fw-bold'>Spotify Recommendations</Navbar.Brand></Link>
                     </div>
                     <div className='col d-flex justify-content-end'>
                         <Button variant="primary" onClick={authHandler}>Log in with Spotify</Button>
@@ -25,7 +25,7 @@ const Header = ({ authHandler, auth, logout }) => {
             <Container fluid className="mb-3">
                 <Navbar bg="light">
                     <div className='col'>
-                        <Link to="/" className='text-decoration-none'><Navbar.Brand className='fw-bold'>DiscoverWeeklyU</Navbar.Brand></Link>
+                        <Link to="/" className='text-decoration-none'><Navbar.Brand className='fw-bold'>Spotify Recommendations</Navbar.Brand></Link>
                         <Link to="/top" className='text-decoration-none text-body'>Top Artists/Tracks</Link>
                     </div>
                     <div className='col d-flex justify-content-end'>
